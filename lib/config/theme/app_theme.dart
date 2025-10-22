1768bbd9f4cc17f82c1f86349b741ba79e3ba3c2
erik uribe <erickjosueuribe@gmail.com>
2025-10-21 21:07:36 -0500

    almost finish

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -7,6 +7,8 @@ const colorList = <Color> [
   Colors.yellow,
   Colors.orange,
   Colors.red,
+  Colors.lightGreenAccent,
+  Colors.cyan,
 ];
 
 class AppTheme {
---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,4 @@
+export 'package:widegets_app/presentation/screens/buttons/buttons_screeen.dart';
+export 'package:widegets_app/presentation/screens/cards/cards_screen.dart';
+export 'package:widegets_app/presentation/screens/home/home_screen.dart';
+
